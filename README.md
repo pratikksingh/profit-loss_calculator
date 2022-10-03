1869fae98a9a1aa1c005ed0af9957245c340ee58
Pratik Singh <singhpratik8828@gmail.com>
2022-10-04 00:03:32 +0530

    fixes

---
+++ README.md
@@ -0,0 +1,3 @@
+### Fun with triangles
+
+This app helps you calculate different properties of triangle and also has a quiz. This app is made with VanillaJS, HTML, CSS.
--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ var btn = document.querySelector("#btn");
 function clickhandler() {
   const intialTotalPrice = intialPrice.value * quantity.value;
   const currentTotalPrice = currentPrice.value * quantity.value;
-  const profit = currentTotalPrice - intialTotalPrice;
-  const profitPercnt = (profit / intialTotalPrice) * 100;
-  const loss = intialTotalPrice - currentTotalPrice;
+  const profit = currentPrice.value - intialPrice.value;
+  const profitPercnt = (profit / (intialPrice.value * quantity.value)) * 100;
+  const loss = intialPrice.value - currentPrice.value;
 
   if (currentTotalPrice > intialTotalPrice) {
     output.innerHTML =
       "Your total profit is " +
       profit +
       " and the percentage is " +
-      profitPercnt +
+      Math.round(profitPercnt * 100) / 100 +
       " %";
   } else if (currentTotalPrice === intialTotalPrice) {
     output.innerHTML = "No profit made yet!";
@@ -25,7 +25,7 @@ function clickhandler() {
       "You are suffering a loss of " +
       loss +
       " and the percentage is " +
-      profitPercnt +
+      Math.round(profitPercnt * 100) / 100 +
       " %";
   }
 }
